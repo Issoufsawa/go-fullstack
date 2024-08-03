be636dd0366aa178d033788ce8b6a36bff3549f8
moussa <lelemoussa12@gmail.com>
2024-08-03 16:50:17 +0200

    create and config Mongodb

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Thing = require('./models/thing');
 
 const app = express();
-app.use('/api/stuff', (req, res, next) => {
+app.use(express.json());
+
+
+
+const uri = "mongodb+srv://moussa:lele2329@cluster0.upcjasm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
+async function run() {
+  try {
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Connexion à MongoDB réussie !");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await mongoose.disconnect();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.use( (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 
+    'Origin, X-Requested-With,Content, Accept, content-Type, Authorization' );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST , PUT , DELETE, PATH , OPTIONS');
+    next();
+  
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff', (req, res, next) => {
     const stuff = [
       {
         _id: 'oeihfzeoi',
@@ -19,8 +64,19 @@ app.use('/api/stuff', (req, res, next) => {
         price: 2900,
         userId: 'qsomihvqios',
       },
+      {
+        _id: 'oeihfzeomoihj',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqioj',
+      },
     ];
     res.status(200).json(stuff);
   });
 
 module.exports = app;
+
+
+
